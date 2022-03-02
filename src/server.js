4581431aff38d93f02ef590aee0331a660492e8d
Yunsik Ohm <oys0817@gmail.com>
2022-03-02 08:33:53 -0500

    add response to the GET request

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const PORT = 4000;
 
 const app = express();
 
+app.get("/", () => console.log("Somebody is trying to go home."));
+
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT} 🚀`);
 
